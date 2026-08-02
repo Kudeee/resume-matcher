a6b5b3b2b3248b96bf83a45d048fd6ef4f4d2b1e
kudeee <codysu08@gmail.com>
2026-08-02 15:11:59 +0800

    section detector, change somethings in the extract funcs

--- app/services/parser.py
+++ app/services/parser.py
@@ -3,21 +3,58 @@ from . import extract_utils as eu
 from docx import Document
 
 
+def section_detector(text):
+    SECTION_ALIASES = {
+        "summary": ["summary", "objective", "profile"],
+        "experience": [
+            "experience",
+            "work history",
+            "professional experience",
+            "employment",
+        ],
+        "skills": ["skills", "technical skills", "core competencies"],
+        "education": ["education", "academic background"],
+        "projects": ["projects", "personal projects"],
+    }
+
+    alias_to_canonical = {
+        variant: canonical
+        for canonical, variants in SECTION_ALIASES.items()
+        for variant in variants
+    }
+
+    buckets = {"head": [], **{canon: [] for canon in SECTION_ALIASES}}
+    current_section = "head"
+
+    for section in text:
+        s = section.rstrip(":-–— ").lower()
+        if s in alias_to_canonical:
+            current_section = alias_to_canonical[s]
+            continue
+        else:
+            if s:
+                buckets[current_section].append(section)
+            else:
+                continue
+
+    sections = {**{key: "\n".join(val) for key, val in buckets.items()}}
+
+    return sections
+
+
 def extract_text_from_docx(file_path):
     document = Document(file_path)
-    mul_col = eu.table_document(document)
+    extracted_table_text = eu.table_document(document)
 
-    if mul_col:
-        return mul_col
+    if extracted_table_text:
+        return extracted_table_text
     else:
-        full_doc = [paragraph.text for paragraph in document.paragraphs]
-        print(full_doc)
-
-        return "\n".join(full_doc)
+        return [paragraph.text for paragraph in document.paragraphs]
 
 
 def extract_text_from_pdf(file_path):
     try:
+        full_doc = []
         with pdfplumber.open(file_path) as pdf:
             for page in pdf.pages:
                 boundary = eu.find_column_boundary(page)
